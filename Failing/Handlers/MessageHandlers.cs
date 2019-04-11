7e715c778d6470b627616ff781aa0cafaef2f11e
vlad.pinchuk <pinchuk.vladyslav@gmail.com>
2019-04-11 16:37:44 +0300

    Initial commit

---
+++ Failing/Handlers/MessageHandlers.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using Newtonsoft.Json;
+
+namespace Failing.Handlers
+{
+    public class MessageHandlers
+    {
+
+        public MessageHandlers()
+        {
+        }
+
+        public Task HelloWorld(SQSEvent @event, ILambdaContext context)
+        {
+            return Task.FromResult(JsonConvert.SerializeObject(new { Message = "Hello world!" }));
+        }
+    }
+}
